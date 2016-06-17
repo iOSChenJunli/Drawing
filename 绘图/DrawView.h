b69a673510aa97a62884b3b2484c1f66368a4ed2
iOSChenJunli <614670902@qq.com>
2016-06-17 17:07:11 +0800

    first commit

---
+++ 绘图/DrawView.h
@@ -0,0 +1,13 @@
+//
+//  DrawView.h
+//  绘图
+//
+//  Created by YideNet on 16/5/19.
+//  Copyright © 2016年 CJL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DrawView : UIView
+
+@end
---
+++ 绘图/DrawView.m
@@ -0,0 +1,21 @@
+//
+//  DrawView.m
+//  绘图
+//
+//  Created by YideNet on 16/5/19.
+//  Copyright © 2016年 CJL. All rights reserved.
+//
+
+#import "DrawView.h"
+
+@implementation DrawView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
