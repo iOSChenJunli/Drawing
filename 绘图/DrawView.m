1ce7bf0a46cdcc73633d5f96a8ea378c977c9a37
iOSChenJunli <614670902@qq.com>
2016-06-17 17:08:55 +0800

    first commit

--- 绘图/DrawView.m
+++ 绘图/DrawView.m
@@ -9,13 +9,78 @@
 #import "DrawView.h"
 
 @implementation DrawView
-
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+-(id)initWithFrame:(CGRect)frame{
+    self= [super initWithFrame:frame];
+    if (self) {
+        self.layer.backgroundColor=[UIColor yellowColor].CGColor;
+    }
+    return self;
 }
-*/
+
+-(id)initWithCoder:(NSCoder *)aDecoder{
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        self.backgroundColor=[UIColor yellowColor];
+        
+       }
+    return self;
+}
+-(void)drawRect:(CGRect)rect{
+//    /**
+//     UIKit
+//     */
+//    UIBezierPath *p =[UIBezierPath bezierPathWithOvalInRect:CGRectMake(0, 0, 100, 100)];
+//    [[UIColor blueColor] setFill];
+//    [p fill];
+//    
+//    /**
+//     Core Graphics
+//     */
+//    CGContextRef con = UIGraphicsGetCurrentContext();
+//    CGContextAddEllipseInRect(con, CGRectMake(0, 150, 100, 100));
+//    CGContextSetFillColorWithColor(con, [UIColor redColor].CGColor);
+//    CGContextFillPath(con);
+    /**
+     UIBezierPath 绘制三角形
+     */
+    UIBezierPath *path = [UIBezierPath bezierPath];
+    [path moveToPoint:CGPointMake(20, 20)];
+    [path addLineToPoint:CGPointMake(self.bounds.size.width-40, 20)];
+    [path addLineToPoint:CGPointMake(self.center.x, self.center.y)];
+    [path closePath];
+    path.lineWidth=2.0f;
+    [[UIColor redColor] set];
+    [path fill];
+    
+    [[UIColor blueColor] set];
+    [path stroke];
+    
+    /**
+     UIBezierPath 绘制矩形
+     */
+    
+    UIBezierPath *rectangularPath =[UIBezierPath bezierPath];
+    
+    rectangularPath.lineWidth =2.0f;
+    
+    [rectangularPath moveToPoint:CGPointMake(20, 150)];
+    [rectangularPath addLineToPoint:CGPointMake(200, 150)];
+    [rectangularPath addLineToPoint:CGPointMake(200, 300)];
+    [rectangularPath addLineToPoint:CGPointMake(20, 300)];
+    [rectangularPath closePath];
+    
+    [[UIColor greenColor] set];
+    [rectangularPath stroke];
+    
+    [[UIColor blueColor] set];
+    [rectangularPath fill];
+    
+    
+    
+    
+}
+
+
+
 
 @end
--- 绘图/ViewController.m
+++ 绘图/ViewController.m
@@ -9,19 +9,46 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+{
+    NSTimer *timer;
+}
+@property (nonatomic ,strong)CAShapeLayer *shapeLayer;
 @end
 
 @implementation ViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.shapeLayer =[CAShapeLayer layer];
+    self.shapeLayer.frame =CGRectMake(0, 0, 100, 100);
+    self.shapeLayer.position= self.view.center;
+    self.shapeLayer.fillColor = [UIColor redColor].CGColor;
+    
+    self.shapeLayer.lineWidth = 4.0f;
+    self.shapeLayer.strokeColor = [UIColor greenColor].CGColor;
+    
+    UIBezierPath *circlePath = [UIBezierPath bezierPathWithOvalInRect:CGRectMake(0, 0, 100, 100)];
+    
+    self.shapeLayer.path=circlePath.CGPath;
+    [self.view.layer addSublayer:self.shapeLayer];
+    
+    self.shapeLayer.strokeStart=0;
+    self.shapeLayer.strokeEnd=0;
+    timer=[NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(circleAnimation) userInfo:nil repeats:YES];
+    //[[NSRunLoop mainRunLoop]addTimer:timer forMode:NSDefaultRunLoopMode];
+    
+   
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)circleAnimation{
+    if (self.shapeLayer.strokeEnd<0.75) {
+        self.shapeLayer.strokeStart+=0.01;
+        self.shapeLayer.strokeEnd+=0.02;
+    }else{
+        [timer invalidate];
+    }
 }
 
 @end
